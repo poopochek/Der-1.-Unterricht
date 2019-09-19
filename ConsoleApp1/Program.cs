4a085d755957a4aea43ff61fc74011778c643152
student 04 <stk_lum_04@outlook.de>
2019-09-19 11:26:57 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            int ganzeZahl = 16557;
+            double gleitkommaZahl = 168.246;
+            char Zeichen = 'J';
+            bool wahrheitswert = true;
+
+            Console.WriteLine(message+Environment.NewLine+ganzeZahl);
+            //Console.WriteLine(ganzeZahl);
+            //Console.WriteLine(gleitkommaZahl);
+            //Console.WriteLine(Zeichen);
+            //Console.WriteLine(wahrheitswert);
+
+
+            //(for(int i=1; i<=10; i++)
+            //{
+            //   Console.Write(i+" ");
+            //}
+            int zahl1;
+            int zahl2;
+            int produkt;
+            Console.WriteLine("Eine ganze Zahl eingeben:");
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Noch eine ganze Zahl eingeben:");
+            zahl2 = Convert.ToInt32(Console.ReadLine());
+            produkt = zahl1 * zahl2;
+            Console.WriteLine("Der Produkt ist: " + produkt);
+
+
+        }
+    }
+}
